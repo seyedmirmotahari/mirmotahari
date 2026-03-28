811a1a41ae15abab46d3e52754c2b295e64a9c66
seyedmirmotahari <mirmotahari@edu.ulisboa.pt>
2026-03-28 19:53:14 +0000

    tryting to updated the files

--- portfolio.js
+++ portfolio.js
@@ -1,5 +1,5 @@
 const portfolioItems = [
-    { title: "pp", category: "Design", href: "#", description: "Created for the Faculty of Fine Arts at the University of Lisbon, this series transforms the building into a cinematic space, where each student’s journey unfolds like a film—unique, evolving, and shaped by their own creative path.", shortDescription: "Cinematic journeys, unique paths", images: ["design/pointofview1.jpg", "design/pointofview2.jpg", "design/pointofview3.jpg"], cardImage: "design/pointofview.jpg" },
+    { title: "Point of View", category: "Design", href: "#", description: "Created for the Faculty of Fine Arts at the University of Lisbon, this series transforms the building into a cinematic space, where each student’s journey unfolds like a film—unique, evolving, and shaped by their own creative path.", shortDescription: "Cinematic journeys, unique paths", images: ["design/pointofview1.jpg", "design/pointofview2.jpg", "design/pointofview3.jpg"], cardImage: "design/pointofview.jpg" },
     {
         title: "Mumbai Guidebook",
         category: "Design",
